a9bde1ed1f8ea811c73271ed919023dd21c52850
Trần Văn Khánh <khanhlvt18@gmail.com>
2024-12-05 11:30:56 +0700

    fix db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-  (
-    await mongoose.connect(
-      "mongodb+srv://CNPM:HustFood@project.v4uum.mongodb.net/hustfood"
-    )
-  ).isObjectIdOrHexString(() => console.log("DB connected"));
+  await mongoose
+    .connect("mongodb+srv://CNPM:HustFood@project.v4uum.mongodb.net/hustfood")
+    .then(() => console.log("DB connected"));
 };
